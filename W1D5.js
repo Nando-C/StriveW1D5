796ceface99d77281416367f8cea2ddaec95a757
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-04-16 09:09:40 +0100

    Section JS Basics complete

--- W1D5.js
+++ W1D5.js
@@ -15,62 +15,116 @@
 /* Ex.A
    Create a variable called "test" and assign a string to it.
 */
+/**************************************************************/
+let test = 'This is a String'
+console.log('Ex.A :' ,test )
+/**************************************************************/
 
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
-
+/**************************************************************/
+let sum = 10 + 20
+console.log('Ex.B :' ,sum )
+/**************************************************************/
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
-
+/**************************************************************/
+let random = Math.floor(Math.random() * 20)
+console.log('Ex.C :' ,random )
+/**************************************************************/
 /* Ex.D
     Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
-
+/**************************************************************/
+let me = {
+    name: 'Hernando',
+    surename: 'Crespo',
+    age: 39
+}
+console.log('Ex.D :' ,me )
+/**************************************************************/
 /* Ex.E 
     Programmatically remove the age property from the previously create object.
 */
-
+/**************************************************************/
+delete me.age
+console.log('Ex.E :' ,me )
+/**************************************************************/
 /* Ex.F 
    Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
 */
-
+/**************************************************************/
+me.skills = ['JavaScript', 'Basic Python']
+console.log('Ex.F :' ,me )
+/**************************************************************/
 /* Ex.G 
    Programmatically remove the last skill from the "skills" array inside the "me" object.
 */
-
+/**************************************************************/
+delete me.skills[1]
+console.log('Ex.G :' ,me )
+/**************************************************************/
 // JS Functions
 /* Ex.1
     Write a function called "dice"; it should randomize an integer number between 1 and 6.
 */
+/**************************************************************/
 
+
+console.log('Ex.1 :' , )
+/**************************************************************/
 /* Ex.2 
     Write a function called "whoIsBigger" which receives 2 numbers as parameters and returns the biggest one.
 */
+/**************************************************************/
 
+
+console.log('Ex.2 :' , )
+/**************************************************************/
 /* Ex.3
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
+/**************************************************************/
 
+
+console.log('Ex.3 :' , )
+/**************************************************************/
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
 */
+/**************************************************************/
 
+
+console.log('Ex.4 :' , )
+/**************************************************************/
 /* Ex.5
    Write a function called "onlyLetters" which receives a string as a parameter and returns it removing all the digits.
    Ex.: onlyLetters("I have 4 dogs")  => returns "I have  dogs"
 */
+/**************************************************************/
 
+
+console.log('Ex.5 :' , )
+/**************************************************************/
 /* Ex.6 
    Write a function called "isThisAnEmail" which receives a string as a parameter and returns true if the string is a valid email address.
 */
+/**************************************************************/
 
+
+console.log('Ex.6 :' , )
+/**************************************************************/
 /* Ex.7
    Write a function called "whatDayIsIt" that should return the current day of the week.
 */
+/**************************************************************/
 
+
+console.log('Ex.7 :' , )
+/**************************************************************/
 /* Ex.8
     Write a function called "rollTheDices" which receives a number as a parameter.
     It should invoke the dice() function defined in Ex1 the specified amount of times,
@@ -81,62 +135,114 @@
         values: [3, 3, 4]
     }
 */
+/**************************************************************/
 
+
+console.log('Ex.8 :' , )
+/**************************************************************/
 /* Ex.9
    Write a function called "howManyDays" which receives a date as a parameter and should return the number of days passed since that date.
 */
+/**************************************************************/
 
+
+console.log('Ex.9 :' , )
+/**************************************************************/
 /* Ex.10
    Write a function called "isTodayMyBirthday" which should return true if today's your birthday, false otherwise.
 */
 
 // JS Arrays // Objs
 // NOTE: movies array is defined at the end of this file!
+/**************************************************************/
 
+
+console.log('Ex. 10:' , )
+/**************************************************************/
 /* Ex.11
    Write a function called "deleteProp" which receives an object and a string as parameters, and returns the given object after deleting its property named as the given string.
 */
+/**************************************************************/
 
+
+console.log('Ex.11 :' , )
+/**************************************************************/
 /* Ex.12 
     Write a function called "olderMovie" which finds the oldest movie in the array provided at the end of this file.
 */
+/**************************************************************/
 
+
+console.log('Ex.12 :' , )
+/**************************************************************/
 /* Ex.13
     Write a function called "countMovies" which returns the number of movies contained in the array provided at the end of this file.
 */
+/**************************************************************/
 
+
+console.log('Ex.13 :' , )
+/**************************************************************/
 /* Ex.14
     Write a function called "onlyTheTitles" which creates an array with just the titles of the movies provided in the array at the end of the file.
 */
+/**************************************************************/
 
+
+console.log('Ex.14 :' , )
+/**************************************************************/
 /* Ex.15
    Write a function called "onlyInThisMillennium" which returns only the movies produced in this millennium.
 */
+/**************************************************************/
 
+
+console.log('Ex.15 :' , )
+/**************************************************************/
 /* Ex.16 
     Write a function called "getMovieById" which receives an id as a parameter and returns the movie with the given id.
 */
+/**************************************************************/
 
+
+console.log('Ex.16 :' , )
+/**************************************************************/
 /* Ex.17
     Write a function called "sumAllTheYears" which returns the sum of all the years in which the movies provided have been produced.
 */
+/**************************************************************/
 
+
+console.log('Ex.17 :' , )
+/**************************************************************/
 /* Ex.18
     Write a function called "searchByTitle" which receives a string as a parameter and returns all the movies which contain that string in the title.
 */
+/**************************************************************/
 
+
+console.log('Ex.18 :' , )
+/**************************************************************/
 /* Ex.19
     Write a function called "searchAndDivide" which receives a string as a parameter and returns an object;
     this object should contain an array called "match", made by all the movies which contain the given string in the title,
     and another array "unmatch" with all the remaining ones.
 */
+/**************************************************************/
 
+
+console.log('Ex.19 :' , )
+/**************************************************************/
 /* Ex.20
    Write a function called "removeIndex" which receives a number as a parameter and returns the movies array without the element in the given position.
 */
 
 // [EXTRAS] JS Advanced
+/**************************************************************/
 
+
+console.log('Ex.20 :' , )
+/**************************************************************/
 /* Ex.21
   Create a function called "halfTree" which receives a number as a parameter and builds an "*" half tree with the given height.
   Example:
@@ -145,7 +251,11 @@
   **
   ***
 */
+/**************************************************************/
 
+
+console.log('Ex.21 :' , )
+/**************************************************************/
 /* Ex.22 
   Create a function called "tree" which receives a number as a parameter and builds an "*" tree with the given height.
   Example: 
@@ -154,7 +264,11 @@
    *** 
   *****
 */
+/**************************************************************/
 
+
+console.log('Ex.22 :' , )
+/**************************************************************/
 /* Ex.23
   Create a function called "isItPrime" that receives a number as a parameter and returns true if the given number is a prime number.
 */
